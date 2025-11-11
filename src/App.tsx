f111eb0159d906c59d0d65341705f02a03b5b715
Jack Tubby <jack200034@hotmail.com>
2025-11-11 11:33:50 +0000

    Init project & home page setup

---
+++ src/App.tsx
@@ -0,0 +1,100 @@
+import './App.css'
+import {
+  ArrowFatLineDownIcon,
+  CodeIcon,
+  EnvelopeIcon,
+  GithubLogoIcon,
+  LinkedinLogoIcon,
+} from '@phosphor-icons/react'
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen max-w-[1200px] mx-auto">
+        <nav className="flex justify-between items-center py-8">
+          <h1 className="text-lg font-bold">Jack Tubby</h1>
+          <ul className="flex gap-x-8">
+            <li className="hover:text-purple-600 hover:underline hover:underline-offset-6">
+              <a href="">Home</a>
+            </li>
+            <li className="hover:text-purple-600 hover:underline hover:underline-offset-6">
+              <a href="">Works</a>
+            </li>
+            <li className="hover:text-purple-600 hover:underline hover:underline-offset-6">
+              <a href="">Services</a>
+            </li>
+            <li className="hover:text-purple-600 hover:underline hover:underline-offset-6">
+              <a href="">Contact</a>
+            </li>
+          </ul>
+          <a href="#" className="bg-purple-600 px-8 py-2 text-lg text-white rounded-2xl shadow-2xl">
+            Let's Talk
+          </a>
+        </nav>
+        <section className="flex justify-between items-center min-h-[80vh]">
+          <div className="w-1/2 flex flex-col gap-y-2">
+            <h3 className="text-3xl">Hello, I'm</h3>
+            <h2 className="font-lato text-9xl font-extrabold mb-4">Jack</h2>
+            <p className="max-w-xl text-lg/loose">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium ipsum earum ab adipisci nemo incidunt
+              vero, ex aliquid optio quaerat culpa facilis maiores quibusdam atque ea reiciendis nesciunt!
+            </p>
+          </div>
+          <div className="w-1/2">
+            <div className="flex flex-col gap-y-6 justify-center items-center">
+              <div className="cursor-pointer">
+                <img
+                  src="https://picsum.photos/200"
+                  alt=""
+                  className="w-[200px] h-[100px] border-4 border-white rounded-xl"
+                />
+                <ArrowFatLineDownIcon size={20} className="mx-auto mt-1" />
+                <div className="bg-white rounded-xl px-6 py-3 text-base mt-1 text-center">About Me</div>
+              </div>
+              <div className="ml-20 cursor-pointer">
+                <img
+                  src="https://picsum.photos/200"
+                  alt=""
+                  className="w-[200px] h-[100px] border-4 border-white rounded-xl"
+                />
+                <ArrowFatLineDownIcon size={20} className="mx-auto mt-1" />
+                <div className="bg-white rounded-xl px-6 py-3 text-base mt-1 text-center">Projects</div>
+              </div>
+              <div className="cursor-pointer">
+                <img
+                  src="https://picsum.photos/200"
+                  alt=""
+                  className="w-[200px] h-[100px] border-4 border-white rounded-xl"
+                />
+                <ArrowFatLineDownIcon size={20} className="mx-auto mt-1" />
+                <div className="bg-white rounded-xl px-6 py-3 text-base mt-1 text-center">Contact</div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <div>
+          <ul className="flex gap-x-4">
+            <li className="relative flex flex-col items-center">
+              <a href="#" className="hover:text-purple-600">
+                <EnvelopeIcon size={32} />
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-purple-600">
+                <LinkedinLogoIcon size={32} />
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-purple-600">
+                <GithubLogoIcon size={32} />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <CodeIcon size={32} className="absolute left-80 bottom-50 text-gray-400" />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        lato: ['"Lato"', 'sans-serif'],
+        rubik: ['"Rubik Bubbles"', 'cursive'],
+      },
+      colors: {
+      },
+      borderRadius: {
+        sm: 'var(--radius-sm)',
+        md: 'var(--radius-md)',
+        lg: 'var(--radius-lg)',
+        xl: 'var(--radius-xl)',
+      },
+    },
+  },
+  plugins: [
+    // require("tailwindcss-animate"),
+  ],
+}
