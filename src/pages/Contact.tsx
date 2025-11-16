0195b0cb7c2abad6162e546a6f9cc1211106a72a
Jack Tubby <jack200034@hotmail.com>
2025-11-16 17:10:05 +0000

    Update styling

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,5 @@
-import '../App.css'
-import Nav from '../components/Nav'
+import "../App.css";
+import Nav from "../components/Nav";
 
 function Contact() {
   return (
@@ -9,22 +9,50 @@ function Contact() {
         <div className="border-2 w-1/2 p-8 text-center flex flex-col gap-y-6 rounded-2xl my-12">
           <h1 className="font-semibold text-3xl">Contact Me</h1>
           <p className="mt-4">
-            Feel free to reach out to me via email or connect with me on LinkedIn or GitHub. I'm always open to
-            discussing new projects, collaborations, or opportunities to contribute to exciting ventures.
+            Feel free to reach out to me via the contact form <span className="text-sm italic">(email)</span> or connect
+            with me on LinkedIn or GitHub. I'm always open to discussing new
+            projects, collaborations, or opportunities to contribute to exciting
+            ventures.
           </p>
           <ul className="flex flex-col gap-y-2">
-            <li>
-              <span className="font-semibold">Email:</span> jacktubby1@gmail.com
+            <li className="mb-10">
+              <span className="font-semibold">Contact</span>
+              <form action="" className="flex flex-col gap-y-2 pt-4">
+                <input
+                  type="text"
+                  placeholder="Name"
+                  className="border-2 px-2 py-1 rounded-md"
+                />
+                <input
+                  type="text"
+                  placeholder="Email"
+                  className="border-2 px-2 py-1 rounded-md"
+                />
+                <textarea
+                  name="desc"
+                  placeholder="Information"
+                  className="border-2 px-2 py-1 rounded-md"
+                ></textarea>
+                <button className="">Send</button>
+              </form>
             </li>
             <li>
-              <span className="font-semibold">LinkedIn:</span>{' '}
-              <a href="https://linkedin.com/in/jack-tubby-08334b1b2" target="_blank" rel="noopener noreferrer">
+              <span className="font-semibold">LinkedIn:</span>{" "}
+              <a
+                href="https://linkedin.com/in/jack-tubby-08334b1b2"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 linkedin.com/in/jack-tubby-08334b1b2
               </a>
             </li>
             <li>
-              <span className="font-semibold">GitHub:</span>{' '}
-              <a href="https://github.com/jacktubby" target="_blank" rel="noopener noreferrer">
+              <span className="font-semibold">GitHub:</span>{" "}
+              <a
+                href="https://github.com/jacktubby"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 github.com/jacktubby
               </a>
             </li>
@@ -32,6 +60,6 @@ function Contact() {
         </div>
       </section>
     </div>
-  )
+  );
 }
-export default Contact
+export default Contact;
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,84 +1,102 @@
-import '../App.css'
-import Nav from '../components/Nav'
+import "../App.css";
+import Nav from "../components/Nav";
 
 function Projects() {
   return (
     <div className="min-h-screen max-w-[1200px] mx-auto">
       <Nav />
+      <section className="my-8"></section>
       <section className="my-8">
-      </section>
-      <section className="my-8">
-        <h2 className='text-xl mb-2'>Current Projects</h2>
+        <h2 className="text-xl mb-2">Current Projects</h2>
         <div className="w-full flex flex-col gap-y-6">
           <div className="w-full h-[400px] flex gap-x-6">
             {/* CARD ONE */}
             <div className="w-1/2 border h-[400px] rounded-2xl relative card">
               <div className="absolute bottom-2 left-2 flex flex-col gap-y-2 bg-black/40 backdrop-blur-md p-4 rounded-xl">
-                <h5 className='text-lg text-white font-semibold'>Rate Guard</h5>
+                <h5 className="text-lg text-white font-semibold">Rate Guard</h5>
                 <p className="text-sm text-white">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident officiis debitis quos?
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Provident officiis debitis quos?
                 </p>
-                <button className="text-white rounded-2xl px-4 py-1 max-w-1/2 bg-linear-to-r from-emerald-400 via-teal-500 to-cyan-600 transition-all duration-500 ease-out">Learn More</button>
+                <button className="cursor-pointer text-white rounded-2xl px-4 py-1 max-w-1/2 bg-linear-to-r from-emerald-400 via-teal-500 to-cyan-600 transition-all duration-500 ease-out">
+                  Learn More
+                </button>
               </div>
             </div>
             {/* CARD TWO */}
-            <div className="w-1/2 border h-[400px] rounded-2xl relative">
-              <div className="absolute bottom-2 left-2 flex flex-col gap-y-2">
-                <h5 className='text-lg'>Must Read</h5>
-                <p className="text-sm max-w-1/2">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident officiis debitis quos?
+            <div className="w-1/2 border h-[400px] rounded-2xl relative card">
+              <div className="absolute bottom-2 left-2 flex flex-col gap-y-2 bg-black/40 backdrop-blur-md p-4 rounded-xl">
+                <h5 className="text-lg text-white font-semibold">Must Read</h5>
+                <p className="text-sm text-white">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Provident officiis debitis quos?
                 </p>
-                <button className="bg-purple-600 text-white rounded-2xl px-4 py-1 max-w-1/2">Learn More</button>
+                <button className="cursor-pointer text-white rounded-2xl px-4 py-1 max-w-1/2 bg-linear-to-r from-emerald-400 via-teal-500 to-cyan-600 transition-all duration-500 ease-out">
+                  Learn More
+                </button>
               </div>
             </div>
-            {/* CARD THREE */}
           </div>
           <div className="w-full max-h-[600px] flex gap-x-6">
-            <div className="w-1/3 border h-[600px] rounded-2xl relative">
-              <div className="absolute bottom-2 left-2 flex flex-col gap-y-2">
-                <h5 className='text-lg'>Assembly Theory Life Simulator</h5>
-                <p className="text-sm max-w-1/2">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident officiis debitis quos?
+            {/* CARD THREE */}
+            <div className="w-1/3 border h-[600px] rounded-2xl relative card">
+              <div className="absolute bottom-2 left-2 flex flex-col gap-y-2 bg-black/40 backdrop-blur-md p-4 rounded-xl">
+                <h5 className="text-lg text-white font-semibold">Assembly Theory Life Simulator</h5>
+                <p className="text-sm text-white">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Provident officiis debitis quos?
                 </p>
-                <button className="bg-purple-600 text-white rounded-2xl px-4 py-1 max-w-1/2">Learn More</button>
+                <button className="cursor-pointer text-white rounded-2xl px-4 py-1 max-w-1/2 bg-linear-to-r from-emerald-400 via-teal-500 to-cyan-600 transition-all duration-500 ease-out">
+                  Learn More
+                </button>
               </div>
             </div>
             <div className="w-1/3 max-h-[600px] flex flex-col gap-y-6">
-              <div className="h-[300px] border rounded-2xl relative">
-                <div className="absolute bottom-2 left-2 flex flex-col gap-y-2">
-                  <h5 className='text-lg'>Telegram Stock Tracker</h5>
-                  <p className="text-sm max-w-1/2">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident officiis debitis quos?
+              {/* CARD FOUR */}
+              <div className="h-[300px] border rounded-2xl relative card">
+                <div className="absolute bottom-2 left-2 flex flex-col gap-y-2 bg-black/40 backdrop-blur-md p-4 rounded-xl">
+                  <h5 className="text-lg text-white font-semibold">Telegram Stock Tracker</h5>
+                  <p className="text-sm text-white">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Provident officiis debitis quos?
                   </p>
-                  <button className="bg-purple-600 text-white rounded-2xl px-4 py-1 max-w-1/2">Learn More</button>
+                  <button className="cursor-pointer text-white rounded-2xl px-4 py-1 max-w-1/2 bg-linear-to-r from-emerald-400 via-teal-500 to-cyan-600 transition-all duration-500 ease-out">
+                    Learn More
+                  </button>
                 </div>
               </div>
-              <div className="h-[300px] border rounded-2xl relative">
-                <div className="absolute bottom-2 left-2 flex flex-col gap-y-2">
-                  <h5 className='text-lg'>Marine Anchor Catenary Calculator</h5>
-                  <p className="text-sm max-w-1/2">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident officiis debitis quos?
+              {/* CARD FIVE */}
+              <div className="h-[300px] border rounded-2xl relative card">
+                <div className="absolute bottom-2 left-2 flex flex-col gap-y-2 bg-black/40 backdrop-blur-md p-4 rounded-xl">
+                  <h5 className="text-lg text-white font-semibold">Marine Anchor Catenary Calculator</h5>
+                  <p className="text-sm text-white">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Provident officiis debitis quos?
                   </p>
-                  <button className="bg-purple-600 text-white rounded-2xl px-4 py-1 max-w-1/2">Learn More</button>
+                  <button className="cursor-pointer text-white rounded-2xl px-4 py-1 max-w-1/2 bg-linear-to-r from-emerald-400 via-teal-500 to-cyan-600 transition-all duration-500 ease-out">
+                    Learn More
+                  </button>
                 </div>
               </div>
             </div>
-            <div className="w-1/3 border h-[600px] rounded-2xl relative">
-              <div className="absolute bottom-2 left-2 flex flex-col gap-y-2">
-                <h5 className='text-lg'>HOA Management App</h5>
-                <p className="text-sm max-w-1/2">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident officiis debitis quos?
+            {/* CARD SIX */}
+            <div className="w-1/3 border h-[600px] rounded-2xl relative card">
+              <div className="absolute bottom-2 left-2 flex flex-col gap-y-2 bg-black/40 backdrop-blur-md p-4 rounded-xl">
+                <h5 className="text-lg text-white font-semibold">HOA Management App</h5>
+                <p className="text-sm text-white">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Provident officiis debitis quos?
                 </p>
-                <button className="bg-purple-600 text-white rounded-2xl px-4 py-1 max-w-1/2">Learn More</button>
+                <button className="cursor-pointer text-white rounded-2xl px-4 py-1 max-w-1/2 bg-linear-to-r from-emerald-400 via-teal-500 to-cyan-600 transition-all duration-500 ease-out">
+                  Learn More
+                </button>
               </div>
             </div>
           </div>
         </div>
       </section>
-      <section className="my-8">
-        {/* <h2>Projects</h2> */}
-      </section>
+      <section className="my-8">{/* <h2>Projects</h2> */}</section>
     </div>
-  )
+  );
 }
-export default Projects
+export default Projects;
